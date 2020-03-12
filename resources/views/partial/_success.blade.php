50708da04b38004309f1126cfeb1b4c1e903afcf
Najib lagmah <31076690+najib7@users.noreply.github.com>
2020-03-12 18:33:51 +0100

    delete old views

--- resources/views/partial/_success.blade.php
+++
@@ -1,8 +0,0 @@
-@if(session('success'))
-
-<div class="alert alert-success" role="alert">
-    <button class="close" data-dismiss="alert" aria-hidden="true">x</button>
-    {{ session('success') }}
-</div>
-
-@endif
