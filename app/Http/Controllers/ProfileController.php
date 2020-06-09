cc033920f8513fe2a1ee7018be78858b1888acb3
najib7 <lagmahnajib@gmail.com>
2020-06-09 11:13:19 +0000

    edit profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
@@ -14,6 +16,7 @@ class ProfileController extends Controller
     {
         $this->middleware(['can:edit-profile,user', 'password.confirm'])->only('destroy', 'edit', 'update');
     }
+    
     /**
      * Display the specified resource.
      *
@@ -25,7 +28,7 @@ class ProfileController extends Controller
         $posts    = $user->posts()->orderBy('id', 'desc')->take(5)->get();
         $comments = $user->comments()->orderBy('id', 'desc')->take(5)->get();
 
-        return view('profile.show', compact('user', 'posts', 'comments'));
+        return view('blog.profile.show', compact('user', 'posts', 'comments'));
     }
 
     /**
@@ -36,7 +39,7 @@ class ProfileController extends Controller
      */
     public function edit(User $user)
     {
-        return view('profile.edit', compact('user'));
+        return view('blog.profile.edit', compact('user'));
     }
 
     /**
@@ -49,45 +52,73 @@ class ProfileController extends Controller
     public function update(Request $request, User $user)
     {
         $request->validate([
-            'name'     => ['required', 'string', 'max:60'],
-            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id]
+            'username'      => "required|alpha_dash|max:20|min:3|unique:users,username," . $user->id,
+            'email'         => "required|string|email|max:255|unique:users,email," . $user->id,
+            'profile_image' => "image|mimes:jpeg,png,jpg,gif,svg|max:2048",
+            'gender'        => "required|in:male,female",
+            'first_name'    => "required|min:3|max:20|alpha_dash",
+            'last_name'     => "required|min:3|max:20|alpha_dash",
+            'country'       => "required|" . Rule::in(config('blog.country_list')),
+            'date_of_birth' => "date|before:5 years ago|nullable",
+            'role'          => "in:admin,author,standard",
         ]);
+        
+        // users table
+        $user->username = $request->username;
+        $user->email    = $request->email;
 
-        $user->name = $request->name;
-        $user->email = $request->email;
+        // profiles table
+        $profile                = $user->profile;
+        $profile->first_name    = $request->first_name;
+        $profile->last_name     = $request->last_name;
+        $profile->gender        = $request->gender;
+        $profile->country       = $request->country;
+        $profile->date_of_birth = $request->date_of_birth;
+        $profile->about         = $request->about;
 
+        // upload image
+        if($request->hasFile('profile_image')) {
+
+            $old_image_path = profiles_storage_path($profile->image);
+            if($profile->image && File::exists($old_image_path)) {
+                File::delete($old_image_path);
+            }
+
+            $file = $request->file('profile_image');
+            $fileName = 'profile-' . strtolower($user->username) . time() . '.' . $file->getClientOriginalExtension();
+            $file->storeAs('public/profiles', $fileName);
+            $profile->image = $fileName;
+        }
+
+        // assign role
+        if(Auth::user()->hasRole('admin') && $request->role) {
+            $user->syncRoles($request->role);
+        }
+        
         $user->save();
+        $profile->save();
 
-        return redirect(route('profile.show', $user))->with('success', 'Success !');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        $user->delete();
-        return redirect('/');
+        return redirect(route('profile.show', $user))->with('success', 'user has been saved succesfully !');
     }
 
     public function editPassword(User $user)
     {
         $this->authorize('change-password', $user);
-        return view('profile.editPassword', compact('user'));
+        return view('blog.profile.editPassword', compact('user'));
     }
 
     public function updatePassword(Request $request, User $user)
     {
         $this->authorize('change-password', $user);
+
+
+
         $request->validate([
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password'     => ['required', 'string', 'min:8', 'password'],
+            'new_password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
-        $user = auth()->user();
-        $user->password = Hash::make($request->password);
+        $user->password = Hash::make($request->new_password);
         $user->save();
 
         Auth::logout();
