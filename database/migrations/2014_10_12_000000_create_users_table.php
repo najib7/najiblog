14eb6f32d5117855883fe2783ebc20fcaea7335c
najib7 <lagmahnajib@gmail.com>
2020-06-10 11:20:53 +0000

    add remember token column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->string('password');
             $table->timestamp('last_login')->nullable();
             $table->timestamps();
